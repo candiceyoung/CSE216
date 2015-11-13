aa8dad64edcaaabc682ec44f0450535df081b1bc
Candice Yang <capricorn.young@gmail.com>
2015-11-13 13:51:51 -0800

    update for views.py

--- coursera/coursera/settings.py
+++ coursera/coursera/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# TEMPLATE_DIR = (os.path.join(BASE_DIR, 'html'),)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'video_app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'coursera.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'html'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,3 +102,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- coursera/video_app/views.py
+++ coursera/video_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.template import Context, loader, RequestContext
+from django.shortcuts import render_to_response, redirect
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.exceptions import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView
+from django.template import TemplateDoesNotExist
 
-# Create your views here.
+def homepage(request):
+    return render_to_response('homepage.html')
+    # return HttpResponse("<p>Hello world</p>")
